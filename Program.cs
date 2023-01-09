5dffe9ea59c83b48bb0b858395d3a42f3434a552
miqueasledesma <mickeas98@gmail.com>
2023-01-09 15:12:49 -0300

    practice c#

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Introduce tu Nombre:");
+string nombre = Console.ReadLine();
+string time = DateTime.Now.ToString("h:mm:ss tt");
+Console.WriteLine(nombre + " y la hora es esta " + time);
+
